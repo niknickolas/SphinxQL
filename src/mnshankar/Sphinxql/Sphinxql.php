4830acf6b08d55211bb123aa772485b8fd2b000a
Martin Fjordvald <martin@evilgeniusmedia.org>
2016-03-01 01:08:40 +0800

    Clean up the code

--- src/mnshankar/Sphinxql/Sphinxql.php
+++ src/mnshankar/Sphinxql/Sphinxql.php
@@ -1,24 +1,28 @@
 <?php
 namespace mnshankar\Sphinxql;
+
 class Sphinxql
 {
-    protected $library;   
+    protected $library;
     protected $hits;
+
     public function __construct(\Foolz\SphinxQL\SphinxQL $library)
     {
-        $this->library = $library;                
+        $this->library = $library;
     }
+
     /**
      * return the library SphinxQL object for chaining other calls
      * @return \Foolz\SphinxQL\SphinxQL
      */
     public function query()
     {
-        return $this->library->create($this->library->getConnection());        
+        return $this->library->create($this->library->getConnection());
     }
+
     /**
      * set the hits array
-     * @param array $hits - the array returned by executing the SphinxQL 
+     * @param array $hits - the array returned by executing the SphinxQL
      * @return \mnshankar\Sphinxql\Sphinxql
      */
     public function with($hits)
@@ -26,37 +30,42 @@ class Sphinxql
         $this->hits = $hits;
         return $this;
     }
+
     /**
-     * if name is null, return id's 
+     * if name is null, return id's
      * if name is class (model) return model->get()
      * if name is table return table->get()
      * @param string $name
      * @param string $key, column name that maps to matched id returned by sphinx
      * @return mixed (either array or eloquentcollection)
      */
-    public function get($name=null, $key='id')
-    {                
-        $matchids = array_pluck($this->hits, $key);        
-        if ($name===null)
+    public function get($name = null, $key = 'id')
+    {
+        $matchids = array_pluck($this->hits, $key);
+
+        if ($name === null)
         {
             return $matchids;
-        }        
-        if (class_exists($name))
-        {            
-             $result = call_user_func_array($name . "::whereIn", array($key, $matchids))->get();          
         }
-        else 
+
+        if (class_exists($name))
+        {
+            $result = call_user_func_array($name . "::whereIn", array($key, $matchids))->get();
+        }
+        else
         {
             $result = \DB::table($name)->whereIn($key, $matchids)->get();
-        }        
+        }
+
         return $result;
-    }    
+    }
+
     /**
-     * Execute raw query against the sphinx server 
+     * Execute raw query against the sphinx server
      * @param string $query
      */
-    public function raw( $query )
+    public function raw($query)
     {
-       return $this->library->getConnection()->query($query);
+        return $this->library->getConnection()->query($query);
     }
 }
